8b7bb9f8b9ba1220abb6721e132713511411d8ff
mats1729 <44669423+mats1729@users.noreply.github.com>
2019-06-12 11:17:26 +0200

    Update weatherbit.cpp
    shortened read delay

--- weatherbit.cpp
+++ weatherbit.cpp
@@ -84,7 +84,7 @@ namespace weatherbit {
         volatile int i;
         P12.setDigitalValue(0);
         P12.setDigitalValue(1);
-        for (i = 1; i < 15; i++); // Changed from 20 to 15 for more robust read.
+        for (i = 1; i < 10; i++); // Changed from 20 to 10 for more robust read.
         int b = P13.getDigitalValue();
         for (i = 1; i < 60; i++);
         return b;
